fa0514e4e2c5ae4c7d694b770c5879a2547b9759
Zachary Bull <zachbull1220@gmail.com>
2026-06-07 13:25:32 -0700

    Add .gitignore, update deployment.yaml for metrics sidecar, and enhance monitoring configuration
    - Created .gitignore to exclude unnecessary files and directories.
    - Updated deployment.yaml to add a sidecar container for Minecraft metrics.
    - Introduced monitoring-values.yaml for Prometheus and Grafana configurations.
    - Modified playbook.yml to install Helm and deploy the observability stack.
    - Upgraded instance type in variables.tf for improved performance.

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-output "cs312-ops4-public_ip" {
-  description = "Public IP of the main ops4 instance"
-  value       = aws_instance.cs312-ops4.public_ip
+output "cs312-ops5-public_ip" {
+  description = "Public IP of the main ops5 instance"
+  value       = aws_instance.cs312-ops5.public_ip
 }
